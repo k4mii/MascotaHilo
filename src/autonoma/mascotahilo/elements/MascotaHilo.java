ef3d6b402a59916555fd5a7f94a96d9d83b5ed8e
Camila <mariac.pradac@autonoma.edu.co>
2025-05-01 21:39:00 -0500

    Primer commit - proyecto MascotaHilo

---
+++ src/autonoma/mascotahilo/elements/MascotaHilo.java
@@ -0,0 +1,16 @@
+package autonoma.mascotahilo.elements;
+
+/**
+ *
+ * @author Kamii
+ */
+public class MascotaHilo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
